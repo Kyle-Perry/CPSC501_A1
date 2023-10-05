cd36d8551e40da8f91d73b3c5fb001b09df9f2b4
Kyle Perry <perry.246@gmail.com>
2023-10-04 20:13:39 -0600

    Extract Method refactor in HumanPlayer.playersTurn
    Extract method refactor to remove long method bad code smell in HumanPlayer.playersTurn. Turned lock dice prompt and behavior into its own method that playersTurn invokes

--- src/game/HumanPlayer.java
+++ src/game/HumanPlayer.java
@@ -14,7 +14,6 @@ public class HumanPlayer extends Player{
 
 		int counter = 0;
 		char choice = ' ';
-		int diceSelection = 0;
 
 		this.rollDice(roll);
 		while(counter < 2)
@@ -32,13 +31,7 @@ public class HumanPlayer extends Player{
 					counter++;
 					break;
 				case 'L': case'l':
-					diceSelection = GameInterface.readInt("Enter the dice to be locked (1-3): ");
-					while(diceSelection < 1 || diceSelection > 3)
-					{
-						GameInterface.displayMessage("Number outside of accepted range.\n");
-						diceSelection = GameInterface.readInt("Enter the dice to be locked (1-3): ");	
-					}
-					setLock(diceSelection - 1, !getLockStatus(diceSelection - 1));
+					lockDice();
 					break;
 				case 'S': case's':
 					determineHand();
@@ -54,4 +47,16 @@ public class HumanPlayer extends Player{
 		GameInterface.displayMessage("Your hand is: " + "{" + getADice(0).getValue() + ", " + getADice(1).getValue() + ", " + getADice(2).getValue() + "}\n\n");
 	}
 
+	public void lockDice() {
+		int diceSelection = 0;
+		
+		diceSelection = GameInterface.readInt("Enter the dice to be locked (1-3): ");
+		while(diceSelection < 1 || diceSelection > 3)
+		{
+			GameInterface.displayMessage("Number outside of accepted range.\n");
+			diceSelection = GameInterface.readInt("Enter the dice to be locked (1-3): ");	
+		}
+		setLock(diceSelection - 1, !getLockStatus(diceSelection - 1));
+	}
+	
 }
